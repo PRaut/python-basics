b3ad88419c153e22bebcc691cea88163db213d0c
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-07-11 21:04:04 +0530

    simple math oprators added

---
+++ math.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+#Math
+
+print(50 + 50) #add
+
+print(50 - 50) #subs
+
+print(50 * 50) #mul
+
+print(50 / 50) #divide
+
+print(50 + 50 * 50 - 50 / 50) #PEMDAS
+
+print(50 % 2) #modulo
+
+print(50 ** 2) #exponents
+
+print(50 / 6) # returns quotient with decimal output i.e 8.333333333333334
+
+# To get no leftovers in quotient use //
+
+print(50 // 6) # No leftover returns 8
